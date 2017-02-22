40cd9caf4a7247cb82cae4eedfbebbc75f093c76
Leo <ramosleo12@gmail.com>
2017-02-22 11:52:36 -0400

    models and allowed hosts

--- land7/settings.py
+++ land7/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/Users/leo/Desarrollo/Github/land7/static'
+STATIC_ROOT = '/home/7landing/land7/static'#'/Users/leo/Desarrollo/Github/land7/static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
